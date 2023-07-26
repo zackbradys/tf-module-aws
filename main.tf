951370b51655f6e46b0ae2d7ac5be0453f0bedd6
Eamon Bauman <eamon@eamonbauman.com>
2023-07-26 11:31:02 -0500

    variablized and defaulted things

--- main.tf
+++ main.tf
@@ -1,13 +1,31 @@
+variable "associate_public_ip" {
+  default = true
+}
 variable "public_key" {}
 variable "image" {}
 variable "disk" {}
 variable "instance_type" {}
+variable "volume_encrypted" {
+  default = true
+}
+variable "volume_delete_on_termination" {
+  default = true
+}
 variable "name" {}
 variable "subnet" {}
 variable "vpc_security_group_id" {}
 variable "access_key" {}
 variable "secret_key" {}
 variable "region" {}
+variable "tag_provisioner" {
+  default = "terraform"
+}
+variable "tag_owner" {
+  default = "hobbyfarm"
+}
+variable "volume_type" {
+  default = "gp3"
+}
 
 provider "aws" {
   access_key = var.access_key
@@ -26,17 +44,17 @@ resource "aws_instance" "instance" {
   key_name                    = aws_key_pair.instance_keypair.key_name
   subnet_id                   = var.subnet
   vpc_security_group_ids      = ["${var.vpc_security_group_id}"]
-  associate_public_ip_address = true
+  associate_public_ip_address = var.associate_public_ip
   tags = {
     Name        = "${var.name}"
-    Owner       = "hobbyfarm"
-    provisioner = "terraform"
+    Owner       = "${var.tag_owner}"
+    provisioner = "${var.tag_provisioner}"
   }
   root_block_device {
-    volume_type           = "gp3"
+    volume_type           = "${var.volume_type}"
     volume_size           = var.disk
-    encrypted             = true
-    delete_on_termination = true
+    encrypted             = var.volume_encrypted
+    delete_on_termination = var.delete_volume_on_termination
   }
 }
 
